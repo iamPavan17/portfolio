9872fb69d5df2951f7082728eb8cb0ed9caaa558
Pavan <pavans@geekyants.com>
2022-04-11 18:28:27 +0530

    🧪 Testing netlify forms

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -18,6 +18,7 @@ import {
 } from "./styles";
 
 export default function Contact() {
+  const [name, setName] = React.useState("");
   const {
     register,
     handleSubmit,
@@ -30,7 +31,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encodeUri({ "form-name": "contact", ...data }),
+      body: encodeUri({ "form-name": "contact", ...data, name2: name }),
     })
       .then(() => alert("Success!"))
       .catch((error) => alert(error));
@@ -50,11 +51,26 @@ export default function Contact() {
         </Text>
 
         {/* A little help for the Netlify post-processing bots */}
-        <form name="contact" netlify="true" netlify-honeypot="bot-field" hidden>
+        <form
+          name="contact"
+          netlify="true"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
           <input name="name" />
+          <input type="text" name="name2" />
           <input name="email" />
           <input name="subject" />
           <input name="message" />
+        </form>
+
+        <form>
+          <input
+            type="text"
+            name="name2"
+            onChange={(e) => setName(e.target.value)}
+          />
         </form>
 
         <FormContainer>
